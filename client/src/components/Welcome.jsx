09ef5ab8a59250bcbd71020994105ecffb283a08
Mabast <mabast.niga@yahoo.com>
2022-07-31 23:58:25 -0500

    created loader component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -99,7 +99,7 @@ const Welcome = () => {
               handleChange={handleChange}
             />
             <div className="h-[1px] w-full bg-gray-400 my-2" />
-            {false ? (
+            {true ? (
               <Loader />
             ) : (
               <button
