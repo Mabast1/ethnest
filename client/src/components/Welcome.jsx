7b9dd8d9b9372b1163ee3e0eceea7b12260e2d76
Mabast <mabast.niga@yahoo.com>
2022-07-31 23:37:58 -0500

    created right col and input component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -9,7 +9,18 @@ const Welcome = () => {
   const commonStyles =
     "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-  const connectWallet = () => {};
+  const connectWallet = ({ placeholder, type, name, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      name={name}
+      handleChange={handleChange}
+    />
+  );
+
+  const Input = () => {};
+
+  const handleChange = () => {};
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -36,6 +47,52 @@ const Welcome = () => {
             <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
             <div className={commonStyles}>Low Fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  0xadlkj....iwejslao
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Keyword (Gif)"
+              name="keyword"
+              type="text"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              handleChange={handleChange}
+            />
           </div>
         </div>
       </div>
