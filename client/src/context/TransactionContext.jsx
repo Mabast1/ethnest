345c79976476cfc11aae154ef879e7a693f40867
Mabast <mabast.niga@yahoo.com>
2022-08-02 01:19:55 -0500

    created context provider

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
-const TransactionContext = () => {
-  return <div>TransactionContext</div>;
+import { contractABI, contractAddress } from "../utils/constants";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+
+  console.log({ provider, signer, transactionContract });
 };
 
-export default TransactionContext;
+export const TransactionProvider = ({ childern }) => {
+  return <TransactionContext.Provider>{childern}</TransactionContext.Provider>;
+};
