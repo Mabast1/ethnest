316387e8a05f352ee4e2c1c1cb69a5da78e0da19
Mabast <mabast.niga@yahoo.com>
2022-08-05 22:19:56 -0500

    created transactionsexist function and passed it to the context provider

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -67,6 +67,19 @@ export const TransactionProvider = ({ children }) => {
     }
   };
 
+  const checkIfTransactionsExist = async () => {
+    try {
+      const transactionContract = getEthereumContract();
+      const transactionCount = await transactionContract.getTransactionCount();
+
+      window.localStorage.setItem("transactionCount", transactionCount);
+    } catch (errors) {
+      console.log(error);
+
+      throw new Error("No ethereum object.");
+    }
+  };
+
   const connectWallet = async () => {
     try {
       if (!ethereum) return alert("Please install and configure MetaMask!");
@@ -132,6 +145,7 @@ export const TransactionProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    checkIfTransactionsExist();
   }, []);
 
   return (
@@ -145,6 +159,7 @@ export const TransactionProvider = ({ children }) => {
         sendTransaction,
         isLoading,
         setIsLoading,
+        checkIfTransactionsExist,
       }}
     >
       {children}
